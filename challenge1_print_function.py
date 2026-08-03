575c8aa39d8d7a3012300b4bb12dcc8dacdd5e40
khadijabintekhalid1-ship-it <khadijabintekhalid1@gmail.com>
2026-08-03 22:35:57 +0500

    Create challenge1_print_function.py

---
+++ challenge1_print_function.py
@@ -0,0 +1,13 @@
+# Part 1 - Basic print statements
+print("Fatima")
+print("Cisco Python Essentials 1")
+
+# Part 2 - sep parameter
+print('28', 'July', '2026', sep='-')
+
+# Part 3 - \n newline character
+print("Hello\nWorld")
+
+# Part 4 - end parameter
+print("Hello", end=" ")
+print("World")
